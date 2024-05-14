1be52f47d504eb46cfead9c3fd4eb8e0ce276840
robbie <rbnas69@gmail.com>
2024-05-14 11:03:27 +0300

    store merged

--- VehicleServer/DTOs/StorekeeperDTO.cs
+++ VehicleServer/DTOs/StorekeeperDTO.cs
@@ -1,11 +1,14 @@
-﻿namespace VehicleServer.DTOs
+﻿using VehicleServer.Entities;
+
+namespace VehicleServer.Controllers
 {
-    public class StoreKeeperDTO
+    public class StoreKeeperDto
     {
         public int StoreKeeperId { get; set; }
-        public string? Name { get; set; }
-        public string? Email { get; set; }
-        public int StoreId { get; set; }
-        public string? StoreName { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+        //public int StoreId { get; set; }
+        //public Store Store { get; set; }
     }
 }
