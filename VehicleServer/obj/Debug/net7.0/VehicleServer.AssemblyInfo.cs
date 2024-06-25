72441960147cc3e2ad1efb9bf085d55fb173c76a
E-Pegasus <41442093+E-Pegasus@users.noreply.github.com>
2024-06-25 00:57:22 -0700

    home page counts

--- VehicleServer/obj/Debug/net7.0/VehicleServer.AssemblyInfo.cs
+++ VehicleServer/obj/Debug/net7.0/VehicleServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehicleServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aa6feb3cdd047ff3fce4983129870a5c67fe74b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb6d54ede0c4610330040cc9878bcaef2d193ca5")]
 [assembly: System.Reflection.AssemblyProductAttribute("VehicleServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehicleServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
